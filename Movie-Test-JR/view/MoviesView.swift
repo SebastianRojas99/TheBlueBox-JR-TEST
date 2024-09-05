fea4db228a8b7c34f88c72dee23c991f1fd83aa9
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-09-05 20:55:32 -0300

    styling

--- Movie-Test-JR/view/MoviesView.swift
+++ Movie-Test-JR/view/MoviesView.swift
@@ -13,33 +13,36 @@ struct MoviesView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVStack(alignment: .leading) { // Usa LazyVStack aquí para optimizar la lista
+                LazyVStack(alignment:.leading){
                     ForEach(moviesVM.movies) { item in
                         NavigationLink {
                             MovieDescription(movie: item)
                         } label: {
-                            HStack {
-                                if let posterPath = item.backdrop_path {
-                                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")) { image in
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 100, height: 150)
-                                    } placeholder: {
-                                        ProgressView()
+                            VStack(alignment:.leading){
+                                HStack {
+                                    if let posterPath = item.backdrop_path {
+                                        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 100, height: 150)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
                                     }
+                                    VStack(alignment: .leading) {
+                                        Text(item.title)
+                                            .font(.title)
+                                            .foregroundStyle(.black)
+                                        Text(item.overview)
+                                            .font(.subheadline)
+                                            .foregroundStyle(.gray.opacity(0.9))
+                                    }
+                                    .padding(.leading, 10)
                                 }
-                                VStack(alignment: .leading) {
-                                    Text(item.title)
-                                        .font(.title)
-                                        .foregroundStyle(.black)
-                                    Text(item.overview)
-                                        .font(.subheadline)
-                                        .foregroundStyle(.gray.opacity(0.9))
-                                }
-                                .padding(.leading, 10) // Añadir padding a la izquierda si es necesario
+                                .padding()
                             }
-                            .padding() // Padding general para cada item
+                            
                         }
                     }
                 }
