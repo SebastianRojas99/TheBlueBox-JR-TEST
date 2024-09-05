24878d223e629ddb102d10b388dc358c39d67331
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-09-05 10:33:13 -0300

    Initial Commit

---
+++ Movie-Test-JR/Movie_Test_JRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Test_JRApp.swift
+//  Movie-Test-JR
+//
+//  Created by Sebastian Marquez Rojas on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Test_JRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
