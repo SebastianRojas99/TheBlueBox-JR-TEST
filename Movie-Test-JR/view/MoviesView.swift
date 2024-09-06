4e51acdc16c7e14f88f1bbc97ef367958f04046e
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-09-06 08:41:09 -0300

    Movies view changed to list

--- Movie-Test-JR/view/MoviesView.swift
+++ Movie-Test-JR/view/MoviesView.swift
@@ -11,41 +11,33 @@ struct MoviesView: View {
     @State private var moviesVM = MoviesVM()
     
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                LazyVStack(alignment:.leading){
-                    ForEach(moviesVM.movies) { item in
-                        NavigationLink {
-                            MovieDescription(movie: item)
-                        } label: {
-                            VStack(alignment:.leading){
-                                HStack {
-                                    if let posterPath = item.backdrop_path {
-                                        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")) { image in
-                                            image
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame(width: 100, height: 150)
-                                        } placeholder: {
-                                            ProgressView()
-                                        }
-                                    }
-                                    VStack(alignment: .leading) {
-                                        Text(item.title)
-                                            .font(.title)
-                                            .foregroundStyle(.black)
-                                        Text(item.overview)
-                                            .font(.subheadline)
-                                            .foregroundStyle(.gray.opacity(0.9))
-                                    }
-                                    .padding(.leading, 10)
-                                }
-                                .padding()
+        NavigationStack{
+            List(moviesVM.movies) { item in
+                NavigationLink{
+                    MovieDescription(movie: item)
+                }label: {
+                    HStack(alignment: .top, spacing: 10) {
+                        if let posterPath = item.backdrop_path {
+                            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 150)
+                            } placeholder: {
+                                ProgressView()
                             }
-                            
+                        }
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(item.title)
+                                .font(.title)
+                                .foregroundColor(.black)
+                            Text(item.overview)
+                                .font(.subheadline)
+                                .foregroundColor(.gray.opacity(0.9))
                         }
                     }
                 }
+                
             }
             .navigationTitle("Films")
         }
